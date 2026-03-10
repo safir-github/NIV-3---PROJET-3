6cde2778347ad2901fae5f769ddbee8ebb11832e
Safir lemoudaa <safirlemoudaa@192.168.1.14>
2026-03-10 00:46:06 +0000

    Configuration des routes de Reservation.

--- routes/catwayRoutes.js
+++ routes/catwayRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const catwayController = require("../controllers/catwayController");
+const reservationController = require("../controllers/reservationController");
 
 router.get("/catways", catwayController.getAllCatways);
 router.get("/catways/:id", catwayController.getCatwayById);
@@ -8,5 +9,12 @@ router.post("/catways", catwayController.createCatway);
 router.put("/catways/:id", catwayController.updateCatway);
 router.patch("/catways/:id", catwayController.patchCatway);
 router.delete("/catways/:id", catwayController.deleteCatway);
+
+// Routes pour les réservations (sous-ressource)
+router.get('/catways/:catwayNumber/reservations', reservationController.getReservationsByCatway);
+router.get('/catways/:catwayNumber/reservations/:id', reservationController.getReservationById);
+router.post('/catways/:catwayNumber/reservations', reservationController.createReservation);
+router.delete('/catways/:catwayNumber/reservations/:id', reservationController.deleteReservation);
+
 
 module.exports = router;
